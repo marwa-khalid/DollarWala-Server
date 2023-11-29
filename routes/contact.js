df3ebb0f553905f8c0cef1f473d897f8a4881a23
Marwa Khalid <77286253+marwa-khalid@users.noreply.github.com>
2023-11-29 22:09:45 +0500

    Update contact.js

--- routes/contact.js
+++ routes/contact.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Contact = require("../models/contact");
+const Contact = require("../models/Contact");
 
 router.post('/', async (req, res) => {  
     try {
