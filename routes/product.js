170319f822cd3320f023e56094684c3cd1308874
Marwa <marwakhalid558@gmail.com>
2023-12-09 21:39:39 +0500

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -1,5 +1,4 @@
 const Product = require("../models/Product");
-const { verifyTokenAndAdmin } = require("./verifyToken");
 const multer = require('multer');
 const router = require("express").Router();
 
